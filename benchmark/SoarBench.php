43471a62378f9c83fe29f66090743133bc22adae
yaozm <ityaozm@gmail.com>
2021-07-06 17:40:24 +0800

    Add benchmark tests

---
+++ benchmark/SoarBench.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of the guanguans/laravel-soar.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use Guanguans\SoarPHP\Support\OsHelper;
+
+/**
+ * @BeforeMethods({"setUp"})
+ */
+final class SoarBench
+{
+    /**
+     * @var \Guanguans\LaravelSoar\Soar
+     */
+    private $soar;
+
+    public function setUp(): void
+    {
+        $config = [
+            // 包自带soar 路径或者自定义的 soar 路径
+            '-soar-path' => OsHelper::isWindows() ? __DIR__.'/../vendor/guanguans/soar-php/bin/soar.windows-amd64' : (OsHelper::isMacOS() ? __DIR__.'/../vendor/guanguans/soar-php/bin/soar.darwin-amd64' : __DIR__.'/../vendor/guanguans/soar-php/bin/soar.linux-amd64'),
+            // 测试环境配置
+            '-test-dsn' => [
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'dbname' => 'database',
+                'username' => 'root',
+                'password' => '123456',
+            ],
+            // 日志输出文件
+            '-log-output' => __DIR__.'/soar.log',
+            // 报告输出格式: 默认  markdown [markdown, html, json]
+            '-report-type' => 'html',
+        ];
+
+        $this->soar = new \Guanguans\LaravelSoar\Soar($config);
+    }
+
+    public function benchSyntaxCheck(): void
+    {
+        $this->soar->syntaxCheck('select * from user id = 1;');
+    }
+
+    public function benchPretty(): void
+    {
+        $this->soar->pretty('select * from user;');
+    }
+
+    public function benchMd2html(): void
+    {
+        $this->soar->md2html('## this is a testing.');
+    }
+
+    public function benchFingerPrint(): void
+    {
+        $this->soar->fingerPrint('select * from user where id = 1;');
+    }
+
+    public function benchHelp(): void
+    {
+        $this->soar->help();
+    }
+}
