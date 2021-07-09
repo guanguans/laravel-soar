fac4a444172827f21faf3696d7a8f821d76b06c9
yaozm <ityaozm@gmail.com>
2021-07-09 16:14:00 +0800

    Update php-cs-fixer config file

---
+++ .php-cs-fixer.php
@@ -0,0 +1,11 @@
+<?php
+
+$header = <<<EOF
+This file is part of the guanguans/package-skeleton.
+
+(c) guanguans <ityaozm@gmail.com>
+
+This source file is subject to the MIT license that is bundled.
+EOF;
+
+return require __DIR__.'/.php_cs.dist';
