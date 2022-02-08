bbf06a0312d34e3eabdce7c251243b3826d9d715
yaozm <ityaozm@gmail.com>
2022-02-08 13:11:55 +0800

    Update IDE helper file

--- _ide_helper.php
+++ _ide_helper.php
@@ -15,241 +15,83 @@ namespace {
 }
 
 namespace Illuminate\Database\Query {
+    /**
+     * @method string toSoarScore()
+     * @method void   dumpSoarScore()
+     * @method void   ddSoarScore()
+     * @method string toSoarMdExplain()
+     * @method void   dumpSoarMdExplain()
+     * @method void   ddSoarMdExplain()
+     * @method string toSoarHtmlExplain()
+     * @method void   dumpSoarHtmlExplain()
+     * @method void   ddSoarHtmlExplain()
+     * @method string toSoarPretty()
+     * @method void   dumpSoarPretty()
+     * @method void   ddSoarPretty()
+     * @method string toSoarHelp()
+     * @method void   dumpSoarHelp()
+     * @method void   ddSoarHelp()
+     *
+     * @see \Guanguans\LaravelDumpSql\ServiceProvider
+     * @see \Illuminate\Database\Query\Builder
+     */
     class Builder
     {
-        public function toSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarScore();
-        }
-
-        public function dumpSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarScore();
-        }
-
-        public function ddSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarScore();
-        }
-
-        public function toSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarMdExplain();
-        }
-
-        public function dumpSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarMdExplain();
-        }
-
-        public function ddSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarMdExplain();
-        }
-
-        public function toSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarHtmlExplain();
-        }
-
-        public function dumpSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarHtmlExplain();
-        }
-
-        public function ddSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarHtmlExplain();
-        }
-
-        public function toSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarPretty();
-        }
-
-        public function dumpSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarPretty();
-        }
-
-        public function ddSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarPretty();
-        }
-
-        public function toSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarHelp();
-        }
-
-        public function dumpSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarHelp();
-        }
-
-        public function ddSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarHelp();
-        }
     }
 }
 
 namespace Illuminate\Database\Eloquent {
+    /**
+     * @method string toSoarScore()
+     * @method void   dumpSoarScore()
+     * @method void   ddSoarScore()
+     * @method string toSoarMdExplain()
+     * @method void   dumpSoarMdExplain()
+     * @method void   ddSoarMdExplain()
+     * @method string toSoarHtmlExplain()
+     * @method void   dumpSoarHtmlExplain()
+     * @method void   ddSoarHtmlExplain()
+     * @method string toSoarPretty()
+     * @method void   dumpSoarPretty()
+     * @method void   ddSoarPretty()
+     * @method string toSoarHelp()
+     * @method void   dumpSoarHelp()
+     * @method void   ddSoarHelp()
+     *
+     * @mixin \Illuminate\Database\Query\Builder
+     *
+     * @see \Guanguans\LaravelDumpSql\ServiceProvider
+     * @see \Illuminate\Database\Eloquent\Builder
+     */
     class Builder
     {
-        public function toSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarScore();
-        }
-
-        public function dumpSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarScore();
-        }
-
-        public function ddSoarScore()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarScore();
-        }
-
-        public function toSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarMdExplain();
-        }
-
-        public function dumpSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarMdExplain();
-        }
-
-        public function ddSoarMdExplain()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarMdExplain();
-        }
-
-        public function toSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarHtmlExplain();
-        }
-
-        public function dumpSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarHtmlExplain();
-        }
-
-        public function ddSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarHtmlExplain();
-        }
-
-        public function toSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarPretty();
-        }
-
-        public function dumpSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarPretty();
-        }
-
-        public function ddSoarPretty()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarPretty();
-        }
-
-        public function toSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::toSoarHelp();
-        }
-
-        public function dumpSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::dumpSoarHelp();
-        }
-
-        public function ddSoarHelp()
-        {
-            return \Illuminate\Database\Query\Builder::ddSoarHelp();
-        }
     }
 }
 
 namespace Illuminate\Database\Eloquent\Relations {
+    /**
+     * @method string toSoarScore()
+     * @method void   dumpSoarScore()
+     * @method void   ddSoarScore()
+     * @method string toSoarMdExplain()
+     * @method void   dumpSoarMdExplain()
+     * @method void   ddSoarMdExplain()
+     * @method string toSoarHtmlExplain()
+     * @method void   dumpSoarHtmlExplain()
+     * @method void   ddSoarHtmlExplain()
+     * @method string toSoarPretty()
+     * @method void   dumpSoarPretty()
+     * @method void   ddSoarPretty()
+     * @method string toSoarHelp()
+     * @method void   dumpSoarHelp()
+     * @method void   ddSoarHelp()
+     *
+     * @mixin \Illuminate\Database\Eloquent\Builder
+     *
+     * @see \Guanguans\LaravelDumpSql\ServiceProvider
+     * @see \Illuminate\Database\Eloquent\Relations\Relation
+     */
     class Relation
     {
-        public function toSoarScore()
-        {
-            return \Illuminate\Database\Eloquent\Builder::toSoarScore();
-        }
-
-        public function dumpSoarScore()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarScore();
-        }
-
-        public function ddSoarScore()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarScore();
-        }
-
-        public function toSoarMdExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::toSoarMdExplain();
-        }
-
-        public function dumpSoarMdExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::dumpSoarMdExplain();
-        }
-
-        public function ddSoarMdExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarMdExplain();
-        }
-
-        public function toSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::toSoarHtmlExplain();
-        }
-
-        public function dumpSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::dumpSoarHtmlExplain();
-        }
-
-        public function ddSoarHtmlExplain()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarHtmlExplain();
-        }
-
-        public function toSoarPretty()
-        {
-            return \Illuminate\Database\Eloquent\Builder::toSoarPretty();
-        }
-
-        public function dumpSoarPretty()
-        {
-            return \Illuminate\Database\Eloquent\Builder::dumpSoarPretty();
-        }
-
-        public function ddSoarPretty()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarPretty();
-        }
-
-        public function toSoarHelp()
-        {
-            return \Illuminate\Database\Eloquent\Builder::toSoarHelp();
-        }
-
-        public function dumpSoarHelp()
-        {
-            return \Illuminate\Database\Eloquent\Builder::dumpSoarHelp();
-        }
-
-        public function ddSoarHelp()
-        {
-            return \Illuminate\Database\Eloquent\Builder::ddSoarHelp();
-        }
     }
 }
