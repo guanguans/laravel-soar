963d57f0280b9075088dede57f67b6c38d59004d
yaozm <ityaozm@gmail.com>
2022-04-19 12:48:22 +0800

    Add NullOutput

---
+++ src/Outputs/NullOutput.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the guanguans/laravel-soar.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\LaravelSoar\Outputs;
+
+use Guanguans\LaravelSoar\Contracts\Output;
+use Illuminate\Support\Collection;
+
+class NullOutput implements Output
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function output(Collection $scores, $event)
+    {
+        // noop
+    }
+}
