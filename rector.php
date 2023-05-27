af72ae00446d00a5b2049d27ec995cd465b7e4ff
yaozm <ityaozm@gmail.com>
2023-05-27 14:44:42 +0800

    Wip 3.x

---
+++ rector.php
@@ -0,0 +1,176 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/soar-php.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use PHPUnit\Framework\TestCase;
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\Expression\InlineIfToExplicitIfRector;
+use Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
+use Rector\CodingStyle\Enum\PreferenceSelfThis;
+use Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector;
+use Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
+use Rector\CodingStyle\Rector\FuncCall\ConsistentPregDelimiterRector;
+use Rector\CodingStyle\Rector\MethodCall\PreferThisOrSelfMethodCallRector;
+use Rector\Config\RectorConfig;
+use Rector\Core\Configuration\Option;
+use Rector\Core\ValueObject\PhpVersion;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveEmptyClassMethodRector;
+use Rector\DeadCode\Rector\MethodCall\RemoveEmptyMethodCallRector;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\Return_\ReturnBinaryAndToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\StmtsAwareInterface\ReturnEarlyIfVariableRector;
+use Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector;
+use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\Rector\MethodCall\RemoveExpectAnyFromMockRector;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\PSR4\Rector\FileWithoutNamespace\NormalizeNamespaceByPSR4ComposerAutoloadRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->importNames(true, false);
+    $rectorConfig->importShortClasses(false);
+    // $rectorConfig->disableParallel();
+    $rectorConfig->parallel(240);
+    $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon');
+    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    // $rectorConfig->cacheClass(FileCacheStorage::class);
+    // $rectorConfig->cacheDirectory(__DIR__.'/build/rector');
+    // $rectorConfig->containerCacheDirectory(__DIR__.'/build/rector');
+    // $rectorConfig->disableParallel();
+    // $rectorConfig->fileExtensions(['php']);
+    // $rectorConfig->indent(' ', 4);
+    // $rectorConfig->memoryLimit('2G');
+    // $rectorConfig->nestedChainMethodCallLimit(3);
+    // $rectorConfig->noDiffs();
+    // $rectorConfig->parameters()->set(Option::APPLY_AUTO_IMPORT_NAMES_ON_CHANGED_FILES_ONLY, true);
+    // $rectorConfig->removeUnusedImports();
+
+    $rectorConfig->bootstrapFiles([
+        // __DIR__.'/vendor/autoload.php',
+    ]);
+
+    $rectorConfig->autoloadPaths([
+        // __DIR__.'/vendor/autoload.php',
+    ]);
+
+    $rectorConfig->paths([
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+        __DIR__.'/.php-cs-fixer.php',
+        __DIR__.'/rector.php',
+        __DIR__.'/examples/soar.options.example.php',
+    ]);
+
+    $rectorConfig->skip([
+        // rules
+        // AddArrayDefaultToArrayPropertyRector::class,
+        // AddDefaultValueForUndefinedVariableRector::class,
+        // AddSeeTestAnnotationRector::class,
+        // CallableThisArrayToAnonymousFunctionRector::class,
+        // ChangeAndIfToEarlyReturnRector::class,
+        // ExplicitBoolCompareRector::class,
+        // RemoveEmptyClassMethodRector::class,
+        // RemoveEmptyMethodCallRector::class,
+        // RemoveUnusedVariableAssignRector::class,
+        // ReturnBinaryOrToEarlyReturnRector::class,
+        // SimplifyBoolIdenticalTrueRector::class,
+        // StaticClosureRector::class,
+        // UnSpreadOperatorRector::class,
+
+        ConsistentPregDelimiterRector::class,
+        EncapsedStringsToSprintfRector::class,
+        InlineIfToExplicitIfRector::class,
+        LogicalToBooleanRector::class,
+        ReturnBinaryAndToEarlyReturnRector::class,
+        WrapEncapsedVariableInCurlyBracesRector::class,
+
+        // NormalizeNamespaceByPSR4ComposerAutoloadRector::class => [
+        //     __DIR__.'/src/Support/helpers.php',
+        // ],
+        // RemoveExpectAnyFromMockRector::class => [
+        //     __DIR__.'/tests/Concerns/WithDumpableTest.php',
+        // ],
+        // ReturnEarlyIfVariableRector::class => [
+        //     __DIR__.'/src/Support/EscapeArg.php',
+        // ],
+        // UnSpreadOperatorRector::class => [
+        //     __DIR__.'/src/Concerns/WithDumpable.php',
+        // ],
+        // StaticClosureRector::class => [
+        //     __DIR__.'/tests/Concerns/WithRunableTest.php',
+        // ],
+
+        // paths
+        __DIR__.'/tests/AspectMock',
+        '**/Fixture*',
+        '**/Fixture/*',
+        '**/Fixtures*',
+        '**/Fixtures/*',
+        '**/Stub*',
+        '**/Stub/*',
+        '**/Stubs*',
+        '**/Stubs/*',
+        '**/Source*',
+        '**/Source/*',
+        '**/Expected/*',
+        '**/Expected*',
+        '**/__snapshots__/*',
+        '**/__snapshots__*',
+    ]);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+        SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        // SetList::GMAGICK_TO_IMAGICK,
+        // SetList::MYSQL_TO_MYSQLI,
+        SetList::NAMING,
+        // SetList::PRIVATIZATION,
+        SetList::PSR_4,
+        SetList::TYPE_DECLARATION,
+        SetList::EARLY_RETURN,
+        SetList::INSTANCEOF,
+
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_90,
+        // PHPUnitSetList::PHPUNIT80_DMS,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_EXCEPTION,
+        PHPUnitSetList::REMOVE_MOCKS,
+        PHPUnitSetList::PHPUNIT_SPECIFIC_METHOD,
+        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
+        PHPUnitSetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ]);
+
+    $rectorConfig->rules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(
+        PreferThisOrSelfMethodCallRector::class,
+        [
+            TestCase::class => PreferenceSelfThis::PREFER_THIS,
+        ]
+    );
+};
