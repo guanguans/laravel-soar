2a8a473888ef68415c9b83bf2c653ee64f81f3eb
yaozm <ityaozm@gmail.com>
2024-06-06 16:21:22 +0800

    test(commands): add test for ScoreCommand
    - Added a new test file ScoreCommandTest.php
    - Added a test case to check the scores of given SQL statements
    - Used Pest Laravel for testing framework

---
+++ tests/Commands/ScoreCommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/laravel-soar.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Tests\Facades;
+
+use Guanguans\LaravelSoar\Commands\ScoreCommand;
+use Symfony\Component\Console\Command\Command;
+
+use function Pest\Laravel\artisan;
+
+it('can get the scores of the given SQL statements', function (): void {
+    artisan(ScoreCommand::class)
+        ->expectsQuestion('Please input the SQL statements', 'select * from foo; select * from bar;')
+        ->assertExitCode(Command::SUCCESS);
+})->group(__DIR__, __FILE__);
